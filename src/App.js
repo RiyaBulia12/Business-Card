341c2eb5b86693168fd683e60dd509e47300ed8f
Apple <riyabulia12@gmail.com>
2022-10-10 17:19:07 +0530

    setup cra

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Business Card
+      </header>
+    </div>
+  );
+}
+
+export default App;
