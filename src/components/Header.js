7ebbcbb1e9490d7ff408496c934d1916b2cd0292
Apple <riyabulia12@gmail.com>
2022-10-12 18:18:15 +0530

    Add google drive link for photo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import photo from '../assets/img/photo.jpg'
 const Header = () => {
    return (
       <>
          <header >
-            <img src={photo} className="bg-cover w-full rounded-t-xl" alt="profile"/>
+            <img src="https://drive.google.com/uc?export=view&id=1MvO7sRN9db1fFgwZG8JHR43LCuvKk9cM"
+            className="bg-cover w-full rounded-t-xl" alt="profile"/>
          </header>
       </>
    )
